2e8520cbbf266e7346764cc5d2fb1f56f77687e1
Juan José Nieto <juanjo.3ns@gmail.com>
2020-10-11 10:20:10 +0100

    Fix errors and added tensorboard

--- src/main.py
+++ src/main.py
@@ -15,7 +15,10 @@ from customLoader import MinecraftData
 from pprint import pprint
 from os.path import join
 from pathlib import Path
+from scipy.signal import savgol_filter
+from torchvision.utils import make_grid
 from torchvision.transforms import transforms
+from torch.utils.tensorboard import SummaryWriter
 
 from torch.utils.data import DataLoader
 
@@ -48,39 +51,70 @@ model.train()
 train_res_recon_error = []
 train_res_perplexity = []
 
+writer = SummaryWriter(log_dir='../tensorboard/test_1/')
+
+def saveModel(model, optim):
+	path = Path('../weights/last_iteration.pt')
+	torch.save({
+        'state_dict': model.state_dict(),
+		'optimizer': optim},
+		path)
+
+
+valid_originals = next(iter(training_loader))
+valid_originals = valid_originals.to(device)
+
+
+
+
 for i in range(conf['num_training_updates']):
-    for img in training_loader:
-        data = img.to(device)
+    batch = next(iter(training_loader))
+    data = batch.to(device)
 
-        optimizer.zero_grad()
-        vq_loss, data_recon, perplexity = model(data)
-        # recon_error = F.mse_loss(data_recon, data) / mrl.data_variance
-        recon_error = F.mse_loss(data_recon, data)
-        loss = recon_error + vq_loss
-        loss.backward()
+    optimizer.zero_grad()
+    vq_loss, data_recon, perplexity = model(data)
+    # recon_error = F.mse_loss(data_recon, data) / mrl.data_variance
+    recon_error = F.mse_loss(data_recon, data)
+    loss = recon_error + vq_loss
+    loss.backward()
 
-        optimizer.step()
+    optimizer.step()
 
-        train_res_recon_error.append(recon_error.item())
-        train_res_perplexity.append(perplexity.item())
+    train_res_recon_error.append(recon_error.item())
+    train_res_perplexity.append(perplexity.item())
 
-        if (i+1) % 5 == 0:
-            print('%d iterations' % (i+1))
-            print('recon_error: %.3f' % np.mean(train_res_recon_error[-100:]))
-            print('perplexity: %.3f' % np.mean(train_res_perplexity[-100:]))
-            print()
+    writer.add_scalar('Reconstruction Error', recon_error.item(), i)
+    writer.add_scalar('Perplexity', perplexity.item(), i)
+
+    if (i+1) % 100 == 0:
+        print('%d iterations' % (i+1))
+        print('recon_error: %.3f' % np.mean(train_res_recon_error[-100:]))
+        print('perplexity: %.3f' % np.mean(train_res_perplexity[-100:]))
+        print()
+        model.eval()
+        vq_output_eval = model._pre_vq_conv(model._encoder(valid_originals))
+        _, valid_quantize, _, _ = model._vq_vae(vq_output_eval)
+        valid_reconstructions = model._decoder(valid_quantize)
+        grid = make_grid(valid_reconstructions.cpu().data)+0.5
+        writer.add_image('images', grid, i)
+        model.train()
 
 train_res_recon_error_smooth = savgol_filter(train_res_recon_error, 201, 7)
 train_res_perplexity_smooth = savgol_filter(train_res_perplexity, 201, 7)
 
-f = plt.figure(figsize=(16,8))
-ax = f.add_subplot(1,2,1)
-ax.plot(train_res_recon_error_smooth)
-ax.set_yscale('log')
-ax.set_title('Smoothed NMSE.')
-ax.set_xlabel('iteration')
+for i, (r,p) in enumerate(zip(train_res_recon_error_smooth, train_res_perplexity_smooth)):
+    writer.add_scalar('savgol/Reconstruction Error', r, i)
+    writer.add_scalar('savgol/Perplexity', p, i)
 
-ax = f.add_subplot(1,2,2)
-ax.plot(train_res_perplexity_smooth)
-ax.set_title('Smoothed Average codebook usage (perplexity).')
-ax.set_xlabel('iteration')
+saveModel(model, optimizer)
+# f = plt.figure(figsize=(16,8))
+# ax = f.add_subplot(1,2,1)
+# ax.plot(train_res_recon_error_smooth)
+# ax.set_yscale('log')
+# ax.set_title('Smoothed NMSE.')
+# ax.set_xlabel('iteration')
+#
+# ax = f.add_subplot(1,2,2)
+# ax.plot(train_res_perplexity_smooth)
+# ax.set_title('Smoothed Average codebook usage (perplexity).')
+# ax.set_xlabel('iteration')
