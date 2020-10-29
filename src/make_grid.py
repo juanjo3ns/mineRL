eae85c837210a4203d291e0259011ef3c83646db
Juan José Nieto <juanjo.3ns@gmail.com>
2020-10-29 16:30:16 +0100

    Make grid redefined

--- src/make_grid.py
+++ src/make_grid.py
@@ -52,30 +52,22 @@ valid_originals = next(iter(validation_loader))
 valid_originals = valid_originals.to(device)
 
 path_imgs = Path('../images')
-if not os.path.exists(path_imgs / 'top'):
-    os.mkdir(path_imgs / 'top')
-if not os.path.exists(path_imgs / 'bottom'):
-    os.mkdir(path_imgs / 'bottom')
-
-# for i in sorted(os.listdir('../weights/vqvae2_0'), key=lambda x: int(x.split('.')[0])):
-#     print(f"Loading model {i}...")
-#     weights = torch.load(f"../weights/vqvae2_0/{i}")['state_dict']
-#     model.load_state_dict(weights)
-#     _, valid_reconstructions, _, _ = model(valid_originals)
-#     grid = make_grid(valid_reconstructions.cpu().data, normalize=True)
-#     plt.imsave(f"../images/{i.split('.')[0]}.png", grid.permute(1,2,0).numpy())
 
 for i in sorted(os.listdir('../weights/vqvae2_0'), key=lambda x: int(x.split('.')[0])):
     print(f"Loading model {i}...")
     weights = torch.load(f"../weights/vqvae2_0/{i}")['state_dict']
     model.load_state_dict(weights)
-    _,_,_, id_t, id_b, _,_ = model.encode(valid_originals)
+    quant_t, quant_b,_, id_t, id_b, _,_ = model.encode(valid_originals)
+    valid_reconstructions = self.decode(quant_t, quant_b)
+
     id_t = id_t.cpu().numpy()
     id_b = id_b.cpu().numpy()
-    for indices, typ in zip([id_t, id_b],['top', 'bottom']):
+    valid_reconstructions = valid_reconstructions.permute(0,2,3,1)
+    valid_reconstructions = valid_reconstructions.cpu().detach().numpy()
+    for imgs in [valid_reconstructions, id_b, id_t]:
         fig, ax = plt.subplots(2,8, figsize=(16,4))
-        for j,ind in enumerate(indices):
-            ax[int(j/8), int(j%8)].imshow(ind)
+        for j,img in enumerate(imgs):
+            ax[int(j/8), int(j%8)].imshow(img)
             ax[int(j/8), int(j%8)].axis('off')
             ax[int(j/8), int(j%8)].axis("tight")
         name = str(i.split('.')[0]) + '.png'
