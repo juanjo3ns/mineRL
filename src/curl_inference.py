c1142756eaed970f7046592d68ab1ff688cc7402
Juan José Nieto <juanjo.3ns@gmail.com>
2020-11-19 17:34:13 +0100

    train with seq_len 20

--- src/curl_inference.py
+++ src/curl_inference.py
@@ -62,26 +62,55 @@ if conf['curl']['load']:
     weights = torch.load(path_weights / conf['experiment'] / conf['curl']['epoch'])['state_dict']
     curl.load_state_dict(weights)
 
-def save_image(j, i):
-    img = np.concatenate((j[0], j[-1]), axis=1)
+def save_image(img, name):
     fig, ax = plt.subplots()
-    plt.imsave(f'./images/curl_sampled/{i}.png',img)
+    plt.imsave(f'../images/sequences_0/{name}.png',img)
     plt.close()
 
-for i, (current_state, action, reward, next_state, done) in enumerate(data.batch_iter(batch_size=1, num_epochs=1, seq_len=10)):
+def save_fig(img, name):
+    plt.imshow(img)
+    plt.axis('off')
+    plt.savefig(f'../images/inference_attention_1/{name}.svg')
+    plt.close()
+
+for i, (current_state, action, reward, next_state, done) in enumerate(data.batch_iter(batch_size=1, num_epochs=1, seq_len=200)):
     batch = current_state['pov']
 
-    obs_anchor = batch[:,0,:,:,:]
-    obs_pos = batch[:,-1,:,:,:]
+    # obs_anchor = batch[:,0,:,:,:]
+    # obs_pos = batch[:,-1,:,:,:]
+    #
+    # obs_anchor = obs_anchor[5]
 
-    obs_anchor = torch.from_numpy(obs_anchor).float().to(device)
-    obs_pos = torch.from_numpy(obs_pos).float().to(device)
+    for j, b in enumerate(batch[0]):
+        if j%20==0:
+            save_image(b, j)
 
-    obs_anchor = obs_anchor.permute(0,3,1,2)
-    obs_pos = obs_pos.permute(0,3,1,2)
+    # save_image(obs_anchor, 'original_image')
+    # obs_anchor = torch.from_numpy(obs_anchor).unsqueeze(dim=0).float().to(device)
+    #
+    # obs_anchor = obs_anchor.permute(0,3,1,2)
+    #
+    # # part 1
+    # # h, conv = curl.encoder.forward_conv(obs_anchor)
+    # # for j,c in enumerate(conv[0]):
+    # #     img = c.detach().cpu().numpy()
+    # #     save_image(img, j)
+    # # ---------
+    #
+    # # part 2
+    # z_a = curl.encode(obs_anchor)
+    # z = z_a[0][:49].detach().cpu().numpy()
+    # z = z.reshape(7,7)
+    # save_fig(z, 'z_reshaped')
+    # zw = z_a*curl.W
+    # zw = zw.detach().cpu().numpy()
+    # zw = np.diag(zw)[:49]
+    # zw = zw.reshape(7,7)
+    # save_fig(zw, 'zxW')
+    # -----------
+    # embed()
+    break
+    # z_pos = curl.encode(obs_pos, ema=True)
+    # embed()
 
-    z_a = curl.encode(obs_anchor)
-    z_pos = curl.encode(obs_pos, ema=True)
-
-    logits = curl.compute_logits(z_a, z_pos)
-    embed()
+    # logits = curl.compute_logits(z_a, z_pos)
